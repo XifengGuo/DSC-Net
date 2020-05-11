0850e100190d8080dc9069868b8216819462aef1
XifengGuo <guoxifeng1990@163.com>
2020-05-11 23:51:03 +0800

    re-produced the result on ORL. Dealt with whe padding difference between Tensorflow and Pytorch.

--- main.py
+++ main.py
@@ -5,53 +5,77 @@ import torch.nn.functional as F
 import numpy as np
 from post_clustering import spectral_clustering, acc, nmi
 import scipy.io as sio
+import math
 
 
-# class Encoder(nn.Module):
-#     def __init__(self, channels, kernels):
-#         """
-#         :param channels: a list containing all channels including the input image channel (1 for gray, 3 for RGB)
-#         :param kernels:  a list containing all kernel sizes, it should satisfy: len(kernels) = len(channels) - 1.
-#         """
-#         super(Encoder, self).__init__()
-#         assert isinstance(channels, list) and isinstance(kernels, list)
-#         self.layers = []
-#         for i in range(1, len(channels)):
-#             self.layers.append(nn.Conv2d(channels[i - 1], channels[i], kernel_size=kernels[i - 1], stride=2, padding=1))
-#             self.layers.append(nn.ReLU(True))
-#         self.model = nn.Sequential(*self.layers)
-#
-#     def forward(self, x):
-#         return self.model(x)
-#
-#
-# class Decoder(nn.Module):
-#     def __init__(self, channels, kernels):
-#         """
-#         :param channels: a list containing all channels including reconstructed image channel (1 for gray, 3 for RGB)
-#         :param kernels: a list containing all kernel sizes, it should satisfy: len(kernels) = len(channels) - 1.
-#         """
-#         super(Decoder, self).__init__()
-#         assert isinstance(channels, list) and isinstance(kernels, list)
-#         self.layers = []
-#         for i in range(len(channels) - 1):
-#             self.layers.append(nn.ConvTranspose2d(channels[i], channels[i + 1], kernel_size=kernels[i], stride=2,
-#                                                   padding=1, output_padding=1))
-#
-#     def forward(self, x):
-#         for layer in self.layers:
-#             x = F.relu(layer(x))
-#         return x
+class Conv2dSamePad(nn.Module):
+    """Mimics tensorflow's 'SAME' padding.
+    """
+
+    def __init__(self, kernel_size, stride):
+        super(Conv2dSamePad, self).__init__()
+        self.kernel_size = torch.nn.modules.utils._pair(kernel_size)
+        self.stride = torch.nn.modules.utils._pair(stride)
+
+    def forward(self, x):
+        in_width = x.size(2)
+        in_height = x.size(3)
+        out_width = math.ceil(float(in_width) / float(self.stride[0]))
+        out_height = math.ceil(float(in_height) / float(self.stride[1]))
+        pad_along_width = ((out_width - 1) * self.stride[0] + self.kernel_size[0] - in_width)
+        pad_along_height = ((out_height - 1) * self.stride[1] + self.kernel_size[1] - in_height)
+        pad_left = math.floor(pad_along_width / 2)
+        pad_top = math.floor(pad_along_height / 2)
+        pad_right = pad_along_width - pad_left
+        pad_bottom = pad_along_height - pad_top
+        return F.pad(x, [pad_left, pad_right, pad_top, pad_bottom], 'constant', 0)
 
 
-class SelfExpression(nn.Module):
-    def __init__(self, n):
-        super(SelfExpression, self).__init__()
-        self.Coefficient = nn.Parameter(1.0e-8 * torch.ones(n, n, dtype=torch.float32), requires_grad=True)
+class ConvTranspose2dSamePad(nn.Module):
+    """
+    This module implements the "SAME" padding mode as in Tensorflow.
+    A tensor with width w_in, feed it to ConvTranspose2d(ci, co, kernel, stride), the width of output tensor T_nopad:
+        w_nopad = (w_in - 1) * stride + kernel
+    If we use padding, i.e., ConvTranspose2d(ci, co, kernel, stride, padding, output_padding), the width of T_pad:
+        w_pad = (w_in - 1) * stride + kernel - (2*padding - output_padding) = w_nopad - (2*padding - output_padding)
+    Yes, in ConvTranspose2d, more padding, the resulting tensor is smaller, i.e., the padding is actually deleting row/col.
+    If `pad`=(2*padding - output_padding) is odd, Pytorch deletes more columns in the left, i.e., the first ceil(pad/2) and
+    last `pad - ceil(pad/2)` columns of T_nopad are deleted to get T_pad.
+    In contrast, Tensorflow deletes more columns in the right, i.e., the first floor(pad/2) and last `pad - floor(pad/2)`
+    columns are deleted.
+    For the height, Pytorch deletes more rows at top, while Tensorflow at bottom.
+    In practice, we usually want `w_pad = w_in * stride`, i.e., the "SAME" padding mode in Tensorflow,
+    so the number of columns to delete:
+        pad = 2*padding - output_padding = kernel - stride
+    We can solve the above equation and get:
+        padding = ceil((kernel - stride)/2), and
+        output_padding = 2*padding - (kernel - stride) which is either 1 or 0.
+    But to get the same result with Tensorflow, we should delete values by ourselves instead of using padding and
+    output_padding in ConvTranspose2d.
+    To get there, we check the following conditions:
+    If pad = kernel - stride is even, we can directly set padding=pad/2 and output_padding=0 in ConvTranspose2d.
+    If pad = kernel - stride is odd, we can use ConvTranspose2d to get T_nopad, and then delete `pad` rows/columns by
+    ourselves; or we can use ConvTranspose2d to delete `pad - 1` by setting `padding=(pad - 1) / 2` and `ouput_padding=0`
+    and then delete the last row/column of the resulting tensor by ourselves.
+    Here we implement the former case.
+    This module should be called after the ConvTranspose2d module with shared kernel_size and stride values.
+    """
 
-    def forward(self, x):  # shape=[n, d]
-        y = torch.matmul(self.Coefficient, x)
-        return y
+    def __init__(self, kernel_size, stride):
+        super(ConvTranspose2dSamePad, self).__init__()
+        self.kernel_size = torch.nn.modules.utils._pair(kernel_size)
+        self.stride = torch.nn.modules.utils._pair(stride)
+
+    def forward(self, x):
+        in_width = x.size(2)
+        in_height = x.size(3)
+        pad_width = self.kernel_size[0] - self.stride[0]
+        pad_height = self.kernel_size[1] - self.stride[1]
+        pad_left = pad_width // 2
+        pad_right = pad_width - pad_left
+        pad_top = pad_height // 2
+        pad_bottom = pad_height - pad_top
+        return x[:, :, pad_left:in_width - pad_right, pad_top: in_height - pad_bottom]
 
 
 class ConvAE(nn.Module):
@@ -65,10 +89,10 @@ class ConvAE(nn.Module):
         self.encoder = nn.Sequential()
         for i in range(1, len(channels)):
             #  Each layer will divide the size of feature map by 2
+            self.encoder.add_module('pad%d' % i, Conv2dSamePad(kernels[i - 1], 2))
             self.encoder.add_module(
                 'conv%d' % i,
-                nn.Conv2d(channels[i - 1], channels[i], kernel_size=kernels[i - 1], stride=2,
-                          padding=int(kernels[i - 1] / 2))
+                nn.Conv2d(channels[i - 1], channels[i], kernel_size=kernels[i - 1], stride=2)
             )
             self.encoder.add_module('relu%d' % i, nn.ReLU(True))
 
@@ -78,14 +102,24 @@ class ConvAE(nn.Module):
             # Each layer will double the size of feature map
             self.decoder.add_module(
                 'deconv%d' % (i + 1),
-                nn.ConvTranspose2d(channels[i], channels[i + 1], kernel_size=kernels[i], stride=2,
-                                   padding=int(kernels[i] / 2), output_padding=1)
+                nn.ConvTranspose2d(channels[i], channels[i + 1], kernel_size=kernels[i], stride=2)
             )
+            self.decoder.add_module('padd%d' % i, ConvTranspose2dSamePad(kernels[i], 2))
             self.decoder.add_module('relud%d' % i, nn.ReLU(True))
 
     def forward(self, x):
         h = self.encoder(x)
         y = self.decoder(h)
+        return y
+
+
+class SelfExpression(nn.Module):
+    def __init__(self, n):
+        super(SelfExpression, self).__init__()
+        self.Coefficient = nn.Parameter(1.0e-8 * torch.ones(n, n, dtype=torch.float32), requires_grad=True)
+
+    def forward(self, x):  # shape=[n, d]
+        y = torch.matmul(self.Coefficient, x)
         return y
 
 
@@ -119,7 +153,7 @@ class DSCNet(nn.Module):
 
 def train(model,  # type: DSCNet
           x, y, epochs, lr=1e-3, weight_coef=1.0, weight_selfExp=150, device='cuda',
-          alpha=0.04, dim_subspace=12, ro=8):
+          alpha=0.04, dim_subspace=12, ro=8, show=10):
     optimizer = optim.Adam(model.parameters(), lr=lr)
     if not isinstance(x, torch.Tensor):
         x = torch.tensor(x, dtype=torch.float32, device=device)
@@ -133,7 +167,7 @@ def train(model,  # type: DSCNet
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        if epoch % 10 == 0:
+        if epoch % show == 0 or epoch == epochs - 1:
             C = model.self_expression.Coefficient.detach().to('cpu').numpy()
             y_pred = spectral_clustering(C, K, dim_subspace, alpha, ro)
             print('Epoch %02d: loss=%.4f, acc=%.4f, nmi=%.4f' % (epoch, loss.item(), acc(y, y_pred), nmi(y, y_pred)))
@@ -141,10 +175,12 @@ def train(model,  # type: DSCNet
 
 if __name__ == "__main__":
     import argparse
+    import warnings
 
     parser = argparse.ArgumentParser(description='DSCNet')
     parser.add_argument('--db', default='coil20',
                         choices=['coil20', 'coil100', 'orl', 'reuters10k', 'stl'])
+    parser.add_argument('--show-freq', default=10, type=int)
     parser.add_argument('--ae-weights', default=None)
     parser.add_argument('--save-dir', default='results')
     args = parser.parse_args()
@@ -165,7 +201,7 @@ if __name__ == "__main__":
         num_sample = x.shape[0]
         channels = [1, 15]
         kernels = [3]
-        epochs = 30
+        epochs = 50
         weight_coef = 1.0
         weight_selfExp = 150
 
@@ -173,6 +209,7 @@ if __name__ == "__main__":
         alpha = 0.04  # threshold of C
         dim_subspace = 12  # dimension of each subspace
         ro = 8  #
+        warnings.warn("You can uncomment line#64 in post_clustering.py to get better result for this dataset!")
     elif db == 'coil100':
         # load data
         data = sio.loadmat('datasets/COIL100.mat')
@@ -216,11 +253,6 @@ if __name__ == "__main__":
     ae_state_dict = torch.load('pretrained_weights_original/%s.pkl' % db)
     dscnet.ae.load_state_dict(ae_state_dict)
     print("Pretrained ae weights are loaded successfully.")
-    print(dscnet.ae.state_dict())
-    a = np.arange(32 * 32).reshape((1, 1, 32, 32)).astype(np.float)
-    a = torch.tensor(a, dtype=torch.float32)
-    print(a.size())
-    print(dscnet.ae.encoder(a))
-    #
-    # train(dscnet, x, y, epochs, weight_coef=weight_coef, weight_selfExp=weight_selfExp,
-    #       alpha=alpha, dim_subspace=dim_subspace, ro=ro, device='cpu')
+
+    train(dscnet, x, y, epochs, weight_coef=weight_coef, weight_selfExp=weight_selfExp,
+          alpha=alpha, dim_subspace=dim_subspace, ro=ro, show=args.show_freq, device='cpu')
